f4c8cca990dcc32d01c97a50b5a76ab46176cebb
John Yack.ext <j.yack.ext@quadient.com>
2023-08-01 02:28:46 +0200

    ajustement

--- src/config.js
+++ src/config.js
@@ -1,8 +1,6 @@
 const config = {
-   API_URL: process.env.REACT_APP_API_URL || 'http://46.105.28.83:8080',
-   //A changer après
-  //API_URLV2: process.env.REACT_APP_API_URLV2 || 'http://46.105.28.83:5000/api',
+  API_URL: process.env.REACT_APP_API_URL || 'http://46.105.28.83:4000/api',
   API_URLV2: process.env.REACT_APP_API_URLV2 || 'http://46.105.28.83:4000/api',
-   secret : 'sncfihm2023adent'
-  };
-  export default config;
+  secret: 'sncfihm2023adent'
+};
+export default config;
